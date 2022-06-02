832087b169c2724d41612a0719a15e5d8442e95e
Cadu <cadu.coelho@gmail.com>
2022-06-02 01:28:54 -0300

    Water comes below Grass.

--- src/world/tile/terrain/mod.rs
+++ src/world/tile/terrain/mod.rs
@@ -8,8 +8,8 @@ use strum::{Display, EnumCount, EnumIter, EnumVariantNames};
 pub enum BaseTerrain {
     Stone = 0,
     Sand = 1,
-    Grass = 2,
-    Water = 3,
+    Water = 2,
+    Grass = 3,
 }
 
 impl Default for BaseTerrain {
@@ -29,8 +29,8 @@ impl From<usize> for BaseTerrain {
         match n {
             0 => BaseTerrain::Stone,
             1 => BaseTerrain::Sand,
-            2 => BaseTerrain::Grass,
-            3 => BaseTerrain::Water,
+            2 => BaseTerrain::Water,
+            3 => BaseTerrain::Grass,
             _ => panic!(),
         }
     }
